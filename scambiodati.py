e66ccf6fe077a91357ad60d68eda6b7577d8419b
nicveggio01 <nic.veggian@gmail.com>
2025-09-18 11:23:28 +0200

    update

---
+++ scambiodati.py
@@ -0,0 +1,25 @@
+#condivisi
+
+
+p=23
+g=5
+
+
+#segreti
+a=11
+b=8
+
+r= g**a % p
+
+print(r)
+#A da il risultato a B e B da il risultato a A
+
+rb= 16
+
+s= 16**a % p
+
+print(s)
+#chiave uguale per entrambi i numeri 
+
+
+
